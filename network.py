7556d2a699e349a9d9fce4c327601f22c37db7a2
ErolB <eroljb@hotmail.com>
2024-11-29 00:18:27 -0500

    successfully builds network with optimization

--- network.py
+++ network.py
@@ -1,13 +1,27 @@
 import pandas as pd
 import numpy as np
 import random
+import time
+import matplotlib.pyplot as plt
 from utils import *
 
+class InputNode:
+    def __init__(self):
+        self.value = None
+
+    def set_value(self, new_value):
+        self.value = new_value
+
+    def evaluate(self):
+        return self.value
 
 class Neuron:
-    def __init__(self, n_inputs, activation='relu'):
-        self.weights = np.array([random.random()*0.01 for _ in range(n_inputs)])
+    def __init__(self, inputs, activation='relu'):
+        self.previous = inputs
+        self.n_inputs = len(inputs)
+        self.weights = np.array([random.random()*0.01 for _ in range(self.n_inputs)])
         self.bias = random.random() * 0.01
+        # define activation functions
         if activation == 'relu':
             self.activation = relu
         elif activation == 'sigmoid':
@@ -15,37 +29,90 @@ class Neuron:
         else:
             self.activation = linear
 
-    def evaluate(self, inputs):
+    def evaluate(self):
+        inputs = np.array([node.evaluate() for node in self.previous])
         z = np.dot(inputs, self.weights) + self.bias
         return self.activation(z)
 
 
 class Network:
-    def __init__(self, n_neurons, n_inputs, n_outputs, p_connect_hidden=0.3, p_connect_input=0.1, p_connect_output=0.1):
+    def __init__(self, n_neurons, n_inputs, n_outputs, p_connect_hidden=0.5, p_connect_input=0.1, p_connect_output=0.1,
+                 max_distance=None):
+        if max_distance is None:
+            max_distance = int(np.sqrt(n_neurons))
+        self.neurons = {i: None for i in range(n_neurons)}
+        self.input_layer = {i: InputNode() for i in range(n_inputs)}
+        self.output_layer = {i: None for i in range(n_outputs)}
         self.n_neurons = n_neurons
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
-        # define connections in hidden portion
-        self.hidden_connections = np.zeros((n_neurons, n_neurons))
-        for i in range(n_neurons):
-            for j in range(n_neurons):
-                if random.random() <= p_connect_hidden:
-                    self.hidden_connections[i,j] = 1
         # define connections to input layer
         self.input_connections = np.zeros((n_inputs, n_neurons))
         for i in range(n_inputs):
             for j in range(n_neurons):
                 if random.random() <= p_connect_input:
-                    self.input_connections[i,j] = 1
-        # define connection to output layer
+                    self.input_connections[i, j] = 1
+        # define connections to output layer
         self.output_connections = np.zeros((n_outputs, n_neurons))
         for i in range(n_outputs):
             for j in range(n_neurons):
                 if random.random() <= p_connect_output:
-                    self.output_connections[i,j] = 1
+                    self.output_connections[i, j] = 1
+        # define connections in hidden portion
+        self.hidden_connections = np.zeros((n_neurons, n_neurons))
+        for i in range(n_neurons):
+            for j in range(n_neurons):
+                if random.random() <= p_connect_hidden:
+                    test_matrix = copy.copy(self.hidden_connections)
+                    test_matrix[i,j] = 1
+                    # check for cycles
+                    if detect_cycle(test_matrix):
+                        continue
+                    # check maximum path length
+                    max_distance_exceeded = False
+                    for k in range(n_outputs):
+                        connected_nodes = [l for l in range(n_inputs) if self.output_connections[k,l]==1]
+                        for n in connected_nodes:
+                            if max_path_length(n, test_matrix) > max_distance:
+                                max_distance_exceeded = True
+                                break
+                        if max_distance_exceeded:
+                            break
+                    if not max_distance_exceeded:
+                        self.hidden_connections = test_matrix
+
 
     def build_network(self):
-        for i in range(self.n_neurons):
-            total_inputs = self.input_connections[i]
+        # define recursive function
+        def build_neuron(neuron_index):
+            if self.neurons[neuron_index] is not None:
+                return self.neurons[neuron_index]
+            previous_nodes = []
+            for i, input_node in enumerate(self.input_connections[:, neuron_index]):
+                if input_node == 1:
+                    previous_nodes.append(self.input_layer[i])
+            for i, hidden_node in enumerate(self.hidden_connections[neuron_index, :]):
+                if hidden_node == 1:
+                    previous_nodes.append(build_neuron(i))
+            new_neuron = Neuron(previous_nodes)
+            self.neurons[neuron_index] = new_neuron
+            return new_neuron
+        # clear existing neurons
+        self.neurons = {i: None for i in range(self.n_neurons)}
+        # build output neurons
+        for i in range(self.n_outputs):
+            connection_indices = [j for j in range(self.n_neurons) if self.output_connections[i,j]==1]
+            connections_to_output = [build_neuron(j) for j in connection_indices]
+            self.output_layer[i] = Neuron(connections_to_output)
+
+
+
+if __name__ == '__main__':
+    net = Network(50, 5, 2, p_connect_input=0.1, p_connect_hidden=0.2, p_connect_output=0.1)
+    net.build_network()
+
+
+
+
 
 
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import random
+import copy
 
 # activation functions
+
 def sigmoid(z):
     return np.exp(z) / (1 + np.exp(z))
 
@@ -16,4 +18,44 @@ def linear(z):
     return z
 
 # graph functions
+
+def dfs_cycle(node_index, adj_matrix, visited, coverage):
+    if visited[node_index]:
+        return True
+    if coverage[node_index]:
+        return False
+    visited[node_index] = True
+    coverage[node_index] = True
+    next_nodes = [i for i in range(adj_matrix.shape[0]) if adj_matrix[i, node_index]==1]
+    for n in next_nodes:
+        cycle_complete = dfs_cycle(n, adj_matrix, copy.copy(visited), coverage)
+        if cycle_complete:
+            return True
+    return False  # no cycles found
+
+def detect_cycle(adj_matrix):
+    coverage = {i: False for i in range(adj_matrix.shape[0])}
+    for starting_point in range(adj_matrix.shape[0]):
+        visited = {i: False for i in range(adj_matrix.shape[0])}
+        if dfs_cycle(starting_point, adj_matrix, visited, coverage):
+            return True
+    return False
+
+def dfs_distance(node_index, distance, adj_matrix):
+    previous_nodes = [i for i in range(adj_matrix.shape[0]) if adj_matrix[node_index, i] == 1]
+    if len(previous_nodes) == 0:
+        return distance
+    lengths = [dfs_distance(n, distance+1, adj_matrix) for n in previous_nodes]
+    return max(lengths)
+
+def max_path_length(node_index, adj_matrix):
+    return dfs_distance(node_index, 0, adj_matrix)
+
+
+if __name__ == "__main__":
+    adj = [[0,0,0,0],
+           [1,0,0,0],
+           [1,0,0,1],
+           [0,1,0,0]]
+    print(max_path_length(0, np.array(adj)))
 
